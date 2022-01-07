2d4cf1d4080bc73a5f932158efa34f62f5ac7eb8
wuxiaoju <wuxiaoju@pingcap.com>
2022-01-07 17:55:25 +0800

    fix naming

--- pkg/apiserver/slowquery/model.go
+++ pkg/apiserver/slowquery/model.go
@@ -40,7 +40,7 @@ type Model struct {
 	// Detail
 	PrevStmt string `gorm:"column:Prev_stmt" json:"prev_stmt"`
 	Plan     string `gorm:"column:Plan" json:"plan"`
-	PlanJson string `gorm:"column:PlanJson" json:"planJson"`
+	PlanJSON string `gorm:"column:PlanJson" json:"PlanJSON"`
 
 	// Basic
 	IsInternal   int    `gorm:"column:Is_internal" json:"is_internal"`
